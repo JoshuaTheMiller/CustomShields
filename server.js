bbf9c746b76db0d0b2792ac38670560208a1e74f
Joshua Miller <joshdan65@gmail.com>
2021-01-07 16:47:38 -0600

    Remove old server code

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('./app.js');
-
-const port = process.env.PORT || 3100;
-const server = http.createServer(app);
-
-server.listen(port, () => {
-    console.log(`Server started successfully on port ${port}`)
-});
